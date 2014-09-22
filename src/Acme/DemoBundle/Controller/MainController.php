5890b2118c5c8345b9f40c8fb633744b5ee9ed95
Conor <conor@graphicandwebdesign.ie>
2014-09-22 23:25:27 +0100

    First Commit

---
+++ src/Acme/DemoBundle/Controller/MainController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Acme\DemoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MainController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('AcmeDemoBundle:Main:main.html.twig');
+    }
+}
